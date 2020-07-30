e077cf06f3eef1765ff55df8eb3dfa8fa9ec2761
SMT046 <SMT046@210.105.224.171>
2020-07-30 10:04:14 +0900

    진행중인 축제 DB 추가

---
+++ TeamProject_Festival/src/DBtest.sql
@@ -0,0 +1,10 @@
+create table pfestival(
+num number, 
+ftitle varchar2(30),
+fdate varchar2(30),
+flocation varchar2(30),
+ftheme varchar2(30));
+
+insert into pfestival values(1, 'È¸»óÁ¦', '2020-08-10~2020-08-12', '±¤ÁÖ µ¿±¸', 'Èú¸µ');
+
+select * from pfestival;
