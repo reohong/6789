8c2ef53a6b7263f5db55c8a4cb420d8d1842df16
SMT060 <SMT060@210.105.224.7>
2020-07-31 11:58:10 +0900

    장소 상세정보 두등등장

--- TeamProject_Festival/src/Place.sql
+++ TeamProject_Festival/src/Place.sql
@@ -13,11 +13,38 @@ insert into place values('동구','지산낙원','광주 동구 지호로 127번길 17 지산낙원
 insert into place values('동구','라이크 스튜디오','광주 동구 장동 80-4 2층','0504-0905-9172','https://www.spacecloud.kr/space/8513');
 insert into place values('동구','루프탑 파티룸 어라운드더문','광주 동구 계림동 1037 3층,4층','0504-1381-2419','https://www.spacecloud.kr/space/25648');
 insert into place values('동구','SIXBOX','광주 동구 동명로 9 3층','0504-1381-0605','https://www.spacecloud.kr/space/23746');
-insert into place values('서구','efe2','ee','010123777','wwww.efe57.com');
-insert into place values('남구','efe3','ee','010456777','wwww.efe65w.com');
-insert into place values('북구','efe4','ee','010789777','wwww.efe56w.com');
-insert into place values('동구','efe5','ee','010222777','wwww.efeffe.com');
-insert into place values('광산구','efe6','ee','010555777','wwww.e23efw.com');
+insert into place values('동구','광주힐링카페멘토','광주 동구 동명로 19-6 2층','0504-1380-6656','https://www.spacecloud.kr/space/199558');
+insert into place values('동구','천칭자리 연습실','광주 동구 동명로26번길 15-1','0504-1380-8560','https://www.spacecloud.kr/space/19412');
+insert into place values('동구','CLA STUDIO','광주 동구 서남로30 지하1층','0504-0905-1853','https://www.spacecloud.kr/space/10502');
+insert into place values('동구','유오워크 금남점 1인 오피스','광주 동구 금남로4가 2 2~6층','0504-0905-8678','https://www.spacecloud.kr/space/5614');
+insert into place values('동구','광주 연습실 마루','광주 동구 호남동 32-15 지하1층','0504-0905-1366','https://www.spacecloud.kr/space/16666');
+insert into place values('동구','좋은날스튜디오','광주 동구 동명로 19-6 3층','0504-1381-5709','https://www.spacecloud.kr/space/26332');
+insert into place values('동구','천칭자리 공연장','광주 동구 동명동 158-4 지하1층','0504-0905-1174','https://www.spacecloud.kr/space/16638');
+insert into place values('서구','카네기비즈니스센터','광주 서구 상무대로 773 세정아울렛 3층','0504-0905-5258','https://www.spacecloud.kr/space/812');
+insert into place values('서구','책방카페 이상현실','광주광역시 서구 화정동 767-43 1층','0504-0905-4358','https://www.spacecloud.kr/space/14899');
+insert into place values('서구','프로마스터 아카데미 세미나실','광주광역시 서구 상무화원로32번길 30 4층','0504-0905-6088','https://www.spacecloud.kr/space/13905');
+insert into place values('서구','로호봇 광주센터 2인실','광주광역시 서구 상무중앙로 7 5층','0504-0905-8529','https://www.spacecloud.kr/space/9567');
+insert into place values('서구','광주미드나잇파티룸','광주 서구 마륵복개로 147 410호','0504-1381-1663','https://www.spacecloud.kr/space/24435');
+insert into place values('서구','더워크 상무점','광주광역시 서구 상무중앙로78번길 5-6 9층','0504-0905-0722','https://www.spacecloud.kr/space/14544');
+insert into place values('서구','상무지구 랄라짐','광주광역시 서구 마륵로 92 5충','0504-0905-5258','https://www.spacecloud.kr/space/18162');
+insert into place values('서구','배우의발견 연습실','광주광역시 서구 치평동 1310-24 5층','0504-0905-5258','https://www.spacecloud.kr/space/3276');
+insert into place values('서구','배우의발견 연습실','광주광역시 서구 치평동 1310-24 5층','0504-0905-5258','https://www.spacecloud.kr/space/3276');
+insert into place values('남구','하루에','광주광역시 남구 진다리로6번나길 3-2 1층','0504-0905-1506','https://www.spacecloud.kr/space/17185');
+insert into place values('남구','하멜아트홀','광주광역시 남구 봉선1로 70 아이비클럽 지하1층','0504-0905-3012','https://www.spacecloud.kr/space/2783');
+insert into place values('남구','스튜디오봄날','광주 남구 양림동 92-28 3층','0504-1380-0065','https://www.spacecloud.kr/space/26507');
+insert into place values('남구','아웃오브오피스','광주광역시 남구 사동 163 지층','0504-0905-1506','https://www.spacecloud.kr/space/10264');
+insert into place values('남구','뮤직트리 합주실 및 연습실','광주광역시 남구 봉선2동 559-2 라이크빌딩 3층','0504-0905-0449','https://www.spacecloud.kr/space/2852');
+insert into place values('남구','하루에','광주광역시 남구 진다리로6번나길 3-2 1층','0504-0905-1506','https://www.spacecloud.kr/space/17185');
+insert into place values('북구','광주 야외 스튜디오','광주광역시 북구 설죽로 254 옥상','0504-0905-1801','https://www.spacecloud.kr/space/11139');
+insert into place values('북구','광주문흥동동네카페','광주광역시 북구 오치동 1033-5','0504-0905-1801','https://www.spacecloud.kr/space/16431');
+insert into place values('북구','빌리언드림 스튜디오','광주 북구 우치로 104-8 4층 ','0504-1381-0643','https://www.spacecloud.kr/space/23201');
+insert into place values('북구','집쓰파티룸','광주광역시 북구 용봉동 161-11 ','0504-0905-1801','https://www.spacecloud.kr/space/4113');
+insert into place values('북구','공간주중','광주광역시 북구 용봉동 161-11 B1','0504-0905-1801','https://www.spacecloud.kr/space/18784');
+insert into place values('북구','피트커피','광주광역시 북구 문흥동 742-30 북구 우치로 100번길61','0504-0905-1801','https://www.spacecloud.kr/space/16436');
+insert into place values('광산구','레이디벅스파티룸','광주광역시 광산구 왕버들로 172 레이디벅스 ','0504-0905-4862','https://www.spacecloud.kr/space/12683');
+insert into place values('광산구','다온소셜트리 세미나실','광주광역시 광산구 신창로 107 대운빌딩 4층 ','0504-0905-9450','https://www.spacecloud.kr/space/6639');
+insert into place values('광산구','모모','광주광역시 광산구 월계동 878-3 초이스호텔 2층','0504-0905-6528','https://www.spacecloud.kr/space/4072');
+insert into place values('광산구','에스플러스 스페이스','광주광역시 광산구 수완동 672-8 2층-3층','0504-0905-8952','https://www.spacecloud.kr/space/7577');
 
 
 select address,tel,homepage from place where name='efe1'
